dc97d65403641d235c41fb8c9965290aa98b63a9
uiwarrior <arnab.sadhya@gmail.com>
2020-06-03 09:17:06 +0530

    removed static data reference

--- client/src/pages/view.js
+++ client/src/pages/view.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Card } from '../styled-components/card';
-import { userAccountArr } from '../data/staticData'
+// import { userAccountArr } from '../data/staticData'
 const viewDetails = ({props}) => {
 
     const [data, setCardData] = useState([])
     return (
         <Card>
-            {data.map(val => {
+         /*    {data.map(val => {
                 return <CardBody>{val}</CardBody>
-            })}
+            })} */
         </Card>
     )
 }
