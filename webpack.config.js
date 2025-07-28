c570cd6e356aed48e5d627c6fac0c136921a2ca3
UIwarrior <arnab.sadhya@gmail.com>
2025-07-28 12:15:16 -0500

    Update old packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,48 @@
 const path = require('path'),
-    webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
+  webpack = require('webpack'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: ['./index.js'],
-        vendor: ['react', 'react-dom'],
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'js/[name].bundle.js',
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /.js$/,
-                loader: 'source-map-loader',
-                enforce: 'pre',
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: "./index.html"
-        }),
+  entry: {
+    app: ['./index.js'],
+    vendor: ['react', 'react-dom'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'source-map-loader',
+        enforce: 'pre',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
-    devServer: {
-        historyApiFallback: true,
-        // hot: true,
-      }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    // hot: true,
+  },
 };
-
-
-/* {
-    test: /.tsx?$/,
-    loader: 'awesome-typescript-loader',
-    exclude: /node_modules/,
-}, */
